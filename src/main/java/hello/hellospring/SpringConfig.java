004e78536bb09ed7bf364a2ccb06bfbaa0c45a14
좌승의 <jsu373712@naver.com>
2021-04-11 19:25:59 +0900

    스프링 DB 접근 기술들

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,28 +1,46 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
+    private final EntityManager em;
+
+    // Spring Data JPA
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
         this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
+        // 메모리
         // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+
+        // 순수 JDBC
+        // return new JdbcMemberRepository(dataSource);
+
+        // JDBC Template
+        //return new JdbcTemplateMemberRepository(dataSource);
+
+        // JPA
+        //return new JpaMemberRepository(em);
+//    }
 }
